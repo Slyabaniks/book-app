509ee6b46df41c97c15075c5f9d1b9f4c7728de7
sulaimon <anuoluwapokorede001@gmail.com>
2021-09-05 08:58:15 -1100

    Updates

---
+++ precache-manifest.b34e037c2be6b34469e3245ee2b781a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7173005f48f91e78b3d",
+    "url": "/book-app/static/css/main.91c44d5c.chunk.css"
+  },
+  {
+    "revision": "b7173005f48f91e78b3d",
+    "url": "/book-app/static/js/main.b7173005.chunk.js"
+  },
+  {
+    "revision": "b5fffb7b30a690a391ff",
+    "url": "/book-app/static/js/1.b5fffb7b.chunk.js"
+  },
+  {
+    "revision": "7e02ea49838fd11623e6",
+    "url": "/book-app/static/js/runtime~main.7e02ea49.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/book-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/book-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/book-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "7a8bd8e7117c5830b2e9271f75e6b167",
+    "url": "/book-app/index.html"
+  }
+];
